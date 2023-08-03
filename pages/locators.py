5e8c79c0dd9f7735532d07cc1a749aa817cd8267
ifegee <ilya.galiev@gmail.com>
2023-08-03 22:48:43 +0400

    add base page locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,12 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators:
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+# class MainPageLocators:
+#     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class LoginPageLocators:
@@ -25,3 +29,4 @@ class ProductPageLocators:
     BREADCRUMBS_PRODUCT_NAME = (By.CSS_SELECTOR, '.breadcrumb .active')
     PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
     PRODUCT_NAME_ALERT = (By.CSS_SELECTOR, '#messages > div:nth-child(1) strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '.alert-success')
