9731c286e34af8efe584b8070b4a2638cf3feff5
ifegee <ilya.galiev@gmail.com>
2023-08-03 13:04:05 +0400

    add product page locators

--- pages/locators.py
+++ pages/locators.py
@@ -15,3 +15,10 @@ class LoginPageLocators:
     REGISTER_PASSWORD_1 = (By.CSS_SELECTOR, '#id_registration-password1')
     REGISTER_PASSWORD_2 = (By.CSS_SELECTOR, '#id_registration-password2')
     REGISTER_BUTTON = (By.CSS_SELECTOR, '[name="registration_submit"]')
+
+
+class ProductPageLocators:
+    MAIN_PRODUCT_GALLERY = (By.CSS_SELECTOR, '#product_gallery')
+    MAIN_PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    MAIN_PRODUCT_ADD_BASKET_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    BASKET_MINI = (By.CSS_SELECTOR, '.basket-mini')
