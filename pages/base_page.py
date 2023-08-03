2921e605b7c814d190bd424310b728955ea7b994
ifegee <ilya.galiev@gmail.com>
2023-08-03 13:03:48 +0400

    add click and solve_quiz methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+import math
+
+from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
 
 
@@ -16,3 +19,30 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
+    def click(self, how, what):
+        try:
+            self.browser.find_element(how, what).click()
+            return True
+        except NoSuchElementException:
+            return False
+
+    def get_value(self, how, what):
+        try:
+            return self.browser.find_element(how, what).text
+        except NoSuchElementException:
+            return False
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
