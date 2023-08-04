3488a3d919e87489a01b4577e73b48c3dffc5631
ifegee <ilya.galiev@gmail.com>
2023-08-04 16:48:24 +0400

    add tests for user with setup

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,50 @@
 import pytest
+from faker import Faker
 
-from pages.basket_page import BasketPage
+from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
+
+"""
+There is a kind of setup realisation:
+
+class TestLoginFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.product = ProductFactory(title = "Best book created by robot") # создаем по апи
+        self.link = self.product.link
+        yield # после этого ключевого слова начинается teardown
+        # выполнится после каждого теста в классе
+        self.product.delete() # удаляем те данные, которые мы создали
+        
+"""
+
+
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, browser):
+        fake = Faker()
+        link = 'https://selenium1py.pythonanywhere.com/accounts/login/'
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(fake.email(), fake.password(length=12))
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.success_message_is_not_present()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_product_gallery()
+        page.add_product_to_basket()
+        page.compare_prices()
+        page.compare_product_names_main_breadcrumbs()
+        page.compare_product_names_main_alert()
 
 
 @pytest.mark.parametrize('offer_number', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
@@ -34,7 +76,6 @@ def test_guest_cant_see_success_message(browser):
     page.success_message_is_not_present()
 
 
-# @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
@@ -56,6 +97,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-girl-who-played-with-non-fire_203/'
